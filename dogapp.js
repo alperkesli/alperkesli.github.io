aed87d3507eeaa5bd7c637142f72333e92857a8f
alperkesli <keslialper@gmail.com>
2021-03-29 16:34:45 +0300

    initial commit

---
+++ dogapp.js
@@ -0,0 +1,78 @@
+/* const BREEDS_URL =  "https://dog.ceo/api/breeds/image/random";
+
+function addDoggo () {
+    // show loading spinner
+    fetch(BREEDS_URL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const img = document.createElement('img');
+            img.src = data.message;
+            img.alt = 'Cute doggo';            
+            document.querySelector('.doggos').appendChild(img);
+            
+            
+
+
+            // stop showing loading spinner
+        })
+}
+
+document.querySelector('.add-doggo').addEventListener("click", addDoggo);
+ */
+const select = document.querySelector('.breeds');
+const spinner = document.querySelector('.spinner');
+
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+
+fetch(BREEDS_URL)
+.then(function (response) {
+    return response.json(); 
+})
+    .then(function (data) {
+       // console.log(data);
+        const breedsObject = data.message
+        const breedsArray = Object.keys(breedsObject);
+
+        for (let i = 0; i < breedsArray.length; i++) {
+            const option = document.createElement('option');
+            option.value = breedsArray[i];
+            option.innerText = breedsArray[i];
+            select.appendChild(option);
+        }
+    })
+
+    select.addEventListener("change", function
+    (event) {
+     //   console.log(event.target.value);
+     let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+ 
+     getDoggo(url);
+    });
+
+
+    const img = document.querySelector('.dog-img');
+        
+
+    function getDoggo (url) {
+        spinner.classList.add("show");
+        img.classList.remove("show");
+
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function(data) {
+                img.src = data.message;
+                // spinner.classList.remove("show");
+                // img.classList.add("show");
+
+            })
+
+    }
+
+    img.addEventListener("load", function() {
+        spinner.classList.remove("show");
+        img.classList.add("show");
+    })
